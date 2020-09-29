eb221d1bfebc492b353d8304694e012afa319694
Adriel Adonis <arodriguez32@fisher.edu>
2020-09-29 13:41:17 -0400

    added a promise, api and created an infinite loop

--- crypto-frontend/src/App.js
+++ crypto-frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
@@ -6,6 +7,19 @@ import './App.css';
 
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  // using promises here, something has to happen then this will happen
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+      console.log(res.data)
+    })
+  })
+
+
+
   return (
     <div className="App">
       <h1>Let's get this API going!</h1>
