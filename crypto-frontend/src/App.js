2ed574b9502ab7a6a3b679f7df8b0b79c2b17c99
Adriel Adonis <arodriguez32@fisher.edu>
2020-09-29 14:38:27 -0400

    added form and searchbox

--- crypto-frontend/src/App.js
+++ crypto-frontend/src/App.js
@@ -2,10 +2,6 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import './App.css';
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
-
-
-
 function App() {
   const [coins, setCoins] = useState([])
 
@@ -14,15 +10,19 @@ function App() {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
       setCoins(res.data)
-      console.log(res.data)
     })
-  })
-
-
+    .catch(error => console.log(error))
+  }, [])
 
   return (
-    <div className="App">
-      <h1>Let's get this API going!</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search for a coin</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input"/>
+        </form>
+      </div>
+      
     </div>
   );
 }
