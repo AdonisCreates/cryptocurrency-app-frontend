3bb9382fab89d9dc0b2c0ba34ae0062effbfb2c8
Adriel Adonis <arodriguez32@fisher.edu>
2020-09-29 15:46:29 -0400

    added functional -/+ changes, need to style with css

--- crypto-frontend/src/Coin.js
+++ crypto-frontend/src/Coin.js
@@ -1,7 +1,7 @@
 // rface + tab = creates an arrow function, pretty cool
 import React from 'react'
 
-const Coin = ({ name, image, symbol, price, volume}) => {
+const Coin = ({ name, image, symbol, price, volume, priceChange}) => {
     return (
         <div className='coin-container'>
             <div className="coin-row">
@@ -12,11 +12,15 @@ const Coin = ({ name, image, symbol, price, volume}) => {
                 </div>
                 <div className="coin-data"></div>
                 <div className="coin-price">${price}</div>
-                {/* This will display the actual commas between the #s */}
-                <p className="coin-volume">${volume.toLocaleString()}</p>
+                {/* localestring will display the actual commas between the #s */}
+                <p className="coin-volume">${volume.toLocaleString()}
+                </p>
+                {/* using a ternary operator, so if there's a change in the coin's price < 0, it will turn red */}
+                {priceChange < 0 ? (
+                    <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                ) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)
+            }
             </div>
-
-            
         </div>
     )
 }
